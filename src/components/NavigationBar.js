730f6ff98985fcadac78428cc9d378e05fb4f5a9
Zach Harper <zjharper@protonmail.com>
2021-02-25 15:23:01 -0600

    added onClick to navbar-brand logo

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -25,7 +25,7 @@ const NavigationBar = () => {
   return (
     <nav className='navbar navbar-expand-sm'>
       <div className='container'>
-        <Link className='navbar-brand' to='/'>Super Store</Link>
+        <Link className='navbar-brand' to='/' onClick={() => setActive('Home')}>Super Store</Link>
         <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
           <span className='navbar-toggler-icon'><i className='fa fa-bars fa-lg hamburger' aria-hidden="true"></i></span>
         </button>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,7 @@
-import { useState, useEffect } from 'react'
 
 const url = 'https://gp-super-store-api.herokuapp.com/item/list?sortDir=asc'
 
 const Home = () => {
-
-  // const [items, setItems] = useState([])
-
-  // const getItems = async () => {
-  //   const response = await fetch(url)
-  //   const items = await response.json()
-  //   console.log(items)
-  //   setItems(items)
-  // }
-
-  // useEffect(() => {
-  //   getItems()
-  // }, [])
 
   return (
     <div className='container'>
